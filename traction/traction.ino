951b66d2fd5f3d4423b7f6bca68a307cb9d4abd0
Slithering-Snake <azizmisleh@gmail.com>
2019-05-07 10:37:37 -0400

    Removed calculations of tachometer feedback

--- traction/traction.ino
+++ traction/traction.ino
@@ -16,7 +16,7 @@ volatile int servo_tacho2;
 
 void traction_control(); // code for control system
 
-void setup_ADC(); // copied from gordon's code
+//void setup_ADC(); // copied from gordon's code
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,7 +29,7 @@ void setup() {
 
   delay(10000); // wait 10 s
 
-  setup_ADC();
+//  setup_ADC();
 
   while (1) traction_control();
 
@@ -65,44 +65,27 @@ void traction_control(){
   input_A0 = (float)sum / n; // average analog input
   voltage_A0 = input_A0*ADC_to_V;*/
 
-  int n, k, traction_change; // for tacho feedback averaging
-  float wb, wf; // ang. vel. of back wheel + front
+  float tractionChange; // for tacho feedback averaging
+  float backVel, frontVel; // vel. of back wheel + front
   float y, rmax, rmin, r; // slip ratio, real and desired, + erorr
   float kp, kd, ki; // controller gains
   float e, ed, ei; //error for controllers
-  float u; // desired speed from that
-
-  n = 100;
-  wb = 0;
-  wf = 0;
-  for (int i = 0; i < n; i++){
-    wb += DC_tacho;
-    wf += servo_tacho1 + servo_tacho2;
-  }
+  float u; // modifies speed and sends a value to speed control
   
-  wb = wb/n;
-  wf = wf/n/2;
-  
-  if (wb > wf) y = abs((wb-wf)/wb); // traction
-  else y = abs((wb-wf)/wf); // braking
+  if (backVel > frontVel) y = abs((backVel-frontVel)/backVel); // traction
+  else y = abs((backVel-frontVel)/frontVel); // braking
 
   rmin = 0.1;
   rmax = 0.5;
   
-  if ((y < rmin) | (y > rmax)) traction_change = 1;
-  else traction_change = 0;
+  if ((y < rmin) | (y > rmax)) tractionChange = 1;
+  else tractionChange = 0;
 
   kp = 5.0; // limit value for stability is 7.0
 
   r = (rmin + rmax)/2;
-  if (traction_control){
     e = r - y;
-    u = kp*e;
-  }
-
-   
-
-
+    u = kp*e*tractionChange;
 }
 
 
